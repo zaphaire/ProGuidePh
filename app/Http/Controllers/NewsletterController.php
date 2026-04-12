40e15da1f9d4ce5a68b6053285d1cb9e46bb0214
zaphaire <developer@example.com>
2026-04-12 10:03:28 +0800

    Shorter subscription message

--- app/Http/Controllers/NewsletterController.php
+++ app/Http/Controllers/NewsletterController.php
@@ -79,7 +79,7 @@ class NewsletterController extends Controller
             Log::error('Failed to send newsletter confirmation email: ' . $e->getMessage());
         }
 
-        $message = 'Thanks for subscribing! Check your email for confirmation.';
+        $message = 'Thanks for subscribing!';
         
         if ($request->expectsJson()) {
             return response()->json(['message' => $message]);
